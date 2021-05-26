007ab1f956a19c66d125314f4b39fc9808bfd6b7
Rafida <rafidakn@gmail.com>
2021-05-27 00:25:36 +0300

    assets added & formatted

---
+++ src/config/images.js
@@ -0,0 +1,29 @@
+import allianz from "../assets/images/allianz.png";
+import creditshelf from "../assets/images/creditshelf.png";
+import laserhub from "../assets/images/laserhub.png";
+import offersLogo from "../assets/images/offersLogo.svg";
+import rectangle from "../assets/images/rectangle.svg";
+import shutterstockFooter from "../assets/images/shutterstock-footer.png";
+import shutterstockHeader from "../assets/images/shutterstock.png";
+import signUpLogo from "../assets/images/signUpLogo.svg";
+import snubesLogo from "../assets/images/snubes-logo.svg";
+import sourceLogo from "../assets/images/sourceLogo.svg";
+import talixoBlack from "../assets/images/talixo-black.png";
+import talixoGray from "../assets/images/talixo-gray.png";
+import usercentrics from "../assets/images/usercentrics.png";
+
+export const Images = {
+  allianz: allianz,
+  creditshelf: creditshelf,
+  laserhub: laserhub,
+  offersLogo: offersLogo,
+  rectangle: rectangle,
+  shutterstockFooter: shutterstockFooter,
+  shutterstockHeader: shutterstockHeader,
+  signUpLogo: signUpLogo,
+  snubesLogo: snubesLogo,
+  sourceLogo: sourceLogo,
+  talixoBlack: talixoBlack,
+  talixoGray: talixoGray,
+  usercentrics: usercentrics,
+};
